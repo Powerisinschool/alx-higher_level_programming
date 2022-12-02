fa6f12da54744a33264d1b3a683324dc9b256199
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-12-02 18:50:44 +0100

    add main param

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,13 @@
 import calculator_1 as calc
 
-a = 10
-b = 5
+def main():
+    a = 10
+    b = 5
 
-print(f"{a} + {b} = {calc.add(a, b)}")
-print(f"{a} - {b} = {calc.sub(a, b)}")
-print(f"{a} * {b} = {calc.mul(a, b)}")
-print(f"{a} / {b} = {calc.div(a, b)}")
+    print(f"{a} + {b} = {calc.add(a, b)}")
+    print(f"{a} - {b} = {calc.sub(a, b)}")
+    print(f"{a} * {b} = {calc.mul(a, b)}")
+    print(f"{a} / {b} = {calc.div(a, b)}")
+
+if __name__ == '__main__':
+    main()
