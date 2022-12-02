aacb49f839f19a92d3bbeb7b529d62b234c0107e
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-12-02 18:42:42 +0100

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+import calculator_1 as calc
+
+a = 10
+b = 5
+
+print(f"{a} + {b} = {calc.add(a, b)}")
+print(f"{a} - {b} = {calc.sub(a, b)}")
+print(f"{a} * {b} = {calc.mul(a, b)}")
+print(f"{a} / {b} = {calc.div(a, b)}")
