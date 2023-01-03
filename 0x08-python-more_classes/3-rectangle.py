9f3661f6cb1febb1cada752eb13751a2d1523b30
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-03 06:09:39 +0100

    checks

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -65,4 +65,6 @@ class Rectangle:
     return 2 * (self.__width + self.__height)
   
   def __str__(self):
+    if self.__width == 0 or self.__height == 0:
+      return ""
     return (("#" * self.__width) + "\n") * self.__height
