036dd3b59ad1ee260c76eeb24c87ea9102e0dea8
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-03 06:14:56 +0100

    fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -57,9 +57,11 @@ class Rectangle:
     self.__height = value
 
   def area(self):
+    '''Returns the current rectangle area'''
     return (self.__width * self.__height)
 
   def perimeter(self):
+    '''Returns the current rectangle perimeter'''
     if self.__width == 0 or self.__height == 0:
       return 0
     return 2 * (self.__width + self.__height)
