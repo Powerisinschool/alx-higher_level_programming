209fe608bb9946f7f472080e0ce9d5771d38813f
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-03 05:48:49 +0100

    Add preliminary checks for initialization function

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,6 +18,17 @@ class Rectangle:
             TypeError: If 'width' or  'height' is not an integer
             ValueError: if 'width' or  'height' < 0
         '''
+
+    if type(width) is not int:
+      raise TypeError("width must be an integer")
+    if width < 0:
+      raise ValueError("width must be >= 0")
+
+    if type(height) is not int:
+      raise TypeError("height must be an integer")
+    if height < 0:
+      raise ValueError("height must be >= 0")
+
     self.__width = width
     self.__height = height
 
