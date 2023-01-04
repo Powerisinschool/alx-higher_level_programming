31cfa5c3adca9df7073472a4763f4abc4f764e2d
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-04 18:54:43 +0100

    fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""LockedClass Class
+  A locked class
+"""
 
 class LockedClass:
-
+    """Locked class"""
     __slots__ = ["first_name"]
 
     def __init__(self):
+        """__init__
+            do nothing
+        """
         pass
