abd782c219b340cfdd816b665e10faaa9d59c5b5
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-04 20:23:47 +0100

    Task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_basic(self):
+        """ Basic test case for max integer in ordered list """
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_second(self):
+        """ Basic second test case for max integer in list """
+        self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
