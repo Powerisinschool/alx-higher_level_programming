c56e3fdbb588d739718d6c63dee70bb56911de74
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-11 08:06:50 +0100

    Task 0

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+	with open(filename, "w") as f:
+		return f.write(text)
+	
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import json
+
+def from_json_string(my_str):
+	# json.
+	return json.load(my_str)
