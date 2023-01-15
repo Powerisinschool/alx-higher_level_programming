d16a4feb998c4db48cdafc10e21c56bd7ad532b6
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-15 14:26:49 +0100

    fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''This function returns a list containing all\
+     the methods and attributes belonging to an object'''
 
 def lookup(obj):
+	'''Function returning list of all the methods belonging to an object'''
 	return dir(obj)
