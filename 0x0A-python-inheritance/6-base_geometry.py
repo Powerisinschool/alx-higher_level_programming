c313e9b45d26581070f52f41da29582f743ee6f2
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-16 14:18:56 +0100

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This class is a class"""
+
+class BaseGeometry:
+	"""
+    This is an empty class
+    """
+	def area(self):
+		raise Exception("area() is not implemented")
+
