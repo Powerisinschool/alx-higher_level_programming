0957c27d70381b96c0cae32321509de2a99f95bf
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-16 16:32:57 +0100

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+
+""" to_json_string function """
+
 import json
 
+
 def to_json_string(my_obj):
+	"""
+	This converts a python object to a json string
+	"""
 	return json.dumps(my_obj)
