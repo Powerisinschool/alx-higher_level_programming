d746cfc1e8429079253147ce69de54a304eb1b93
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-16 16:30:45 +0100

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+""" append_write function """
+
+
 def append_write(filename="", text=""):
+	"""
+	This function appends text to a file
+	"""
 	with open(filename, "a") as f:
 		return f.write(text)
