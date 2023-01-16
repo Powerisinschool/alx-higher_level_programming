82f6e4fd0a9917eae96e487fcf49c529a8713a9f
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-16 16:28:34 +0100

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+""" write_file function """
+
+
 def write_file(filename="", text=""):
+	""" This function writes a given string to the file specified """
 	with open(filename, "w") as f:
 		return f.write(text)
 	
