89d92fc387f48bec2db35ad5e5031775f325fb99
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-16 19:11:59 +0100

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Base model """
+
+
+class Base:
+	"""
+	This is the base class
+	"""
+	__nb_objects = 0
+
+	def __init__(self, id=None):
+		if id is not None:
+			self.id = id
+		else:
+			self.__nb_objects += 1
+			self.id = self.__nb_objects
