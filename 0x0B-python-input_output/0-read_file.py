6789bafeaf465e633d6cd10ac5cf84cfa2b9b3c3
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-16 16:25:19 +0100

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+""" This is read_file function """
+
+
 def read_file(filename=""):
+	"""
+	This function reads a file
+	"""
 	with open(filename, "r") as f:
 		print(f.read())
