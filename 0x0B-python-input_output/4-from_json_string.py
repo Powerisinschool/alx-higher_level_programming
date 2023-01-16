a81f0d705685b0b44d0be01030ac2b7c18ce7199
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-16 16:39:10 +0100

    Task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
+""" from_json_string function """
+
 import json
 
-def from_json_string(my_str):
-	# json.
-	return json.load(my_str)
+
+def from_json_string(my_str: str) -> dict:
+	"""
+	This function converts a string to a python object
+	"""
+	return json.loads(my_str)
