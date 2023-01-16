0445a83f43208e61e9230d99bfae9d0bcfd779e9
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-16 15:09:58 +0100

    fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,16 +13,19 @@ class BaseGeometry:
 	def integer_validator(self, name, value):
 		if type(value) is not int:
 			raise TypeError(f"{name} must be an integer")
-		elif value <= 0:
+		if value <= 0:
 			raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
-	__width = 0
-	__height = 0
-
+	"""
+	This class inherits from BaseGeometry and creates a rectangle with width and height
+	"""
 	def __init__(self, width, height):
-		BaseGeometry.integer_validator(self, "width", width)
-		BaseGeometry.integer_validator(self, "height", height)
+		"""
+		Initialize the width and height of the rectangle, width and height must be positive integers
+		"""
 		self.__width = width
 		self.__height = height
+		self.integer_validator("width", self.__width)
+		self.integer_validator("height", self.__height)
