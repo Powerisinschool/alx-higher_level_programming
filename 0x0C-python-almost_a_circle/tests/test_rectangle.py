80de0ee3988df3a8790321fcc8471c0a693af23a
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-17 11:37:48 +0100

    Test Cases

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import unittest
+import re
 from models.rectangle import Rectangle
 
 
@@ -12,13 +13,14 @@ class TestBase(unittest.TestCase):
 		with self.assertRaises(AttributeError):
 			print(Rectangle.__nb_objects)
 
-	# def test_functionality(self):
-	# 	r1 = Rectangle(10, 2)
-	# 	r2 = Rectangle(2, 10)
-	# 	r3 = Rectangle(10, 2, 0, 0, 12)
-	# 	self.assertEqual(r1.id, 6)
-	# 	self.assertEqual(r2.id, 7)
-	# 	self.assertEqual(r3.id, 12)
+	def test_functionality(self):
+		r1 = Rectangle(10, 2)
+		r2 = Rectangle(2, 10)
+		r3 = Rectangle(10, 2, 0, 0, 12)
+		r4 = Rectangle(12, 2)
+		self.assertEqual(r2.id, r1.id + 1)
+		self.assertEqual(r3.id, 12)
+		self.assertEqual(r4.id, r1.id + 2)
 
 	def test_raises_error(self):
 		self.assertRaises(TypeError, Rectangle, 10, "2")
@@ -38,4 +40,5 @@ class TestBase(unittest.TestCase):
 	def test_string_representation(self):
 		self.assertEqual(str(Rectangle(4, 6, 2, 1, 12)), "[Rectangle] (12) 2/1 - 4/6")
 		self.assertEqual(str(Rectangle(5, 5, 1))[-9:], "1/0 - 5/5")
-		# self.assertRegex(str(Rectangle(5, 5, 1)), "[Rectangle] (*) 1/0 - 5/5")
+		pattern = re.compile(r"\[Rectangle] \([0-9]+\) 1/0 - 5/5")
+		self.assertRegex(str(Rectangle(5, 5, 1)), pattern)
