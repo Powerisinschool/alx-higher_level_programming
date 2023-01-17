d4c08d963fda523b25f2b1ead69155fb764b43cb
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-17 14:39:53 +0100

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,24 +6,24 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-	"""
-	This is the square class that inherits from Rectangle
-	"""
-	def __init__(self, size, x=0, y=0, id=None):
-		super().__init__(size, size, x, y, id)
+    """
+    This is the square class that inherits from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
 
-	@property
-	def size(self):
-		return self.width
+    @property
+    def size(self):
+        return self.width
 
-	@size.setter
-	def size(self, value):
-		self.width = value
-		self.height = value
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
-	def __str__(self):
-		"""
-		__str__
-		Return the string representation of the square
-		"""
-		return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    def __str__(self):
+        """
+        __str__
+        Return the string representation of the square
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
