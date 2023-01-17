05f99632343b4425c8904e07fc94661adb1fb8ff
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-17 14:42:49 +0100

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,14 +4,14 @@
 
 
 class Base:
-	"""
-	This is the base class
-	"""
-	__nb_objects = 0
+    """
+    This is the base class
+    """
+    __nb_objects = 0
 
-	def __init__(self, id=None):
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
