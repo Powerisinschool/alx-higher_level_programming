424b4cf601bb1dab2cd84359a73c811b5d5921a7
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-17 18:10:07 +0100

    Task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,12 @@ class Base:
         if json_string is None or json_string in ("", "[]"):
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
