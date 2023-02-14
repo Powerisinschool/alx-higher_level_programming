e31f80a9baf7fb4ffce4eb604333ab3caf109fdb
Powerisinschool <toolulopeolagunju@gmail.com>
2023-02-14 18:16:13 +0100

    Task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,13 @@
--- Multiple
+-- creates second_table and adds multiple rows
 CREATE TABLE IF NOT EXISTS second_table (
     id INT DEFAULT NULL,
     name VARCHAR(255) DEFAULT NULL,
     score INT DEFAULT NULL
 );
-INSERT INTO first_table (id, name, score) VALUES (1, "John", 10);
-INSERT INTO first_table (id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO first_table (id, name, score) VALUES (3, "Bob", 14);
-INSERT INTO first_table (id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table
+  (id, name, score)
+VALUES
+  ('1', 'John', '10'),
+  ('2', 'Alex', '3'),
+  ('3', 'Bob', '14'),
+  ('4', 'George', '8');
