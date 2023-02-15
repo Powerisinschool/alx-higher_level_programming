10902b5d0a6937f4a042a5e028d11bf2c6a2aea5
Powerisinschool <toolulopeolagunju@gmail.com>
2023-02-15 15:40:19 +0100

    Task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- import database dump hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Lists all the cities and corresponding states that can be found in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, ciies.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
