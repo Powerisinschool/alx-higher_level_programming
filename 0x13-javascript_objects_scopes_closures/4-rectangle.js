69f94d28c30c2c5168a0355d2ac9a90ef4629776
Powerisinschool <toolulopeolagunju@gmail.com>
2023-03-14 18:19:14 +0100

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      let j = 0;
+
+      for (; j < this.width; ++j) {
+        process.stdout.write('X');
+      }
+
+      if (j === this.width) {
+        console.log('');
+      }
+    }
+  }
+
+  rotate() {
+    const w = this.width;
+    this.width = this.height;
+    this.height = w;
+  }
+
+  double() {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
