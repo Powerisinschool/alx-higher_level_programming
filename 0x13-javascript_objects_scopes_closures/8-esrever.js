5e9de1b0d14d65da95e4dcda68a00f8609f05ee8
Powerisinschool <toolulopeolagunju@gmail.com>
2023-03-14 18:53:27 +0100

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  let i = 0;
+  let j = list.length - 1;
+  while (i < j) {
+    // swap list[i] and list[j]
+    let temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+    // increment i and decrement j
+    i++;
+    j--;
+  }
+  return list;
 }
