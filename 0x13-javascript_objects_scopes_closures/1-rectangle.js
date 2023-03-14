a01f5fbac167e0ddfbdca36e4bc094e084546db6
Powerisinschool <toolulopeolagunju@gmail.com>
2023-03-14 17:24:44 +0100

    Task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle.
+
+module.exports = class Rectangle {
+	constructor(w, h) {
+		this.width = w;
+		this.height = h;
+	}
+}
