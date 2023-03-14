133cb976fa1b0e74d3b245e1f773d50dee88810a
Powerisinschool <toolulopeolagunju@gmail.com>
2023-03-14 18:41:38 +0100

    linting

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 const Square5 = require('./5-square.js');
 
 module.exports = class Square extends Square5 {
-  charPrint(c) {
+  charPrint (c) {
     for (let i = 0; i < this.width; i++) {
       for (let j = 0; j < this.height; j++) {
         process.stdout.write(c || 'X');
