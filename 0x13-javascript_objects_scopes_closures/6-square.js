0ec6b8ce4692aa77cb16bfaa991341c70a336246
Powerisinschool <toolulopeolagunju@gmail.com>
2023-03-14 18:39:19 +0100

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const Square5 = require('./5-square.js');
+
+module.exports = class Square extends Square5 {
+  charPrint(c) {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        process.stdout.write(c || 'X');
+      }
+      console.log('');
+    }
+  }
+};
