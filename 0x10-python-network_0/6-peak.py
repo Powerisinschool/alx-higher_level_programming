4b5508d0f5e0119d5b99790f53a7176d05a62633
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-04-01 15:52:24 -0700

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" function find_peak """
+
+def find_peak(list_of_integers):
+  if len(list_of_integers) == 0:
+    return
+  if len(list_of_integers) == 1:
+    return list_of_integers[0]
+  a = list_of_integers
+  for i in range(1, len(a)):
+    if i == len(a)-1:
+      return a[i] if a[i] >= a[i-1] else None
+    if (a[i] >= a[i-1] and i > 1) and (a[i] >= a[i+1] and i < len(a)):
+        return a[i]
+  if a[0] >= a[1]:
+    return a[0]
